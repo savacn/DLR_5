b3974c5fe74d00fe5b9d3a410f6fd911eec9f770
Savely <112480298+savacn@users.noreply.github.com>
2023-12-18 00:50:33 +0300

    Add files via upload

---
+++ DKR_5_1.pas
@@ -0,0 +1,71 @@
+﻿program DKR_5_1;
+type
+  arr = array[1..100] of Integer;
+var
+  f1, f2: text; 
+  i, n: Integer;
+  a: arr;
+procedure sortselect(var a: arr; n: Integer; ascending: Boolean); 
+var
+  i, j, index, k: Integer;
+begin
+  for i := 1 to n - 1 do
+  begin
+    index := i;
+    for j := i + 1 to n do
+    begin
+      if ascending then
+      begin
+        if a[j] < a[index] then
+          index := j;
+      end
+      else
+      begin
+        if a[j] > a[index] then
+          index := j;
+      end;
+    end;
+
+    if index <> i then
+    begin
+      k := a[i];
+      a[i] := a[index];
+      a[index] := k;
+    end;
+  end;
+end;
+begin
+  assign(f1, 'popit.txt'); 
+  reset(f1);
+  readln(f1, n);
+
+  for i := 1 to n do 
+    read(f1, a[i]);
+
+  close(f1);
+
+  writeln('Выберите порядок сортировки:');
+  writeln('1. По возрастанию');
+  writeln('2. По убыванию');
+  write('Введите номер: ');
+  var choice: Integer;
+  readln(choice);
+  case choice of
+    1: sortselect(a, n, True); 
+    2: sortselect(a, n, False); 
+  else
+    begin
+      writeln('Неверный выбор');
+      Halt;
+    end;
+  end;
+  assign(f2, 'savout1.txt'); 
+  rewrite(f2);
+
+  for i := 1 to n do 
+    write(f2, a[i], ' ');
+
+  close(f2);
+  
+  writeln('Сортировка прошла успешно. Результат записан в файл savout1.txt');
+end.
---
+++ DKR_5_2.pas
@@ -0,0 +1,65 @@
+﻿program DKR_5_2;
+const
+  Sz = 100;
+  K = 4;
+var
+  a, b: array[0..Sz] of Integer;
+  c: array[0..15] of Integer;
+  m, n, i, t: Integer;
+  e: Integer;
+  inputFile, outputFile: Text; 
+procedure RadixSort(i: integer);
+begin
+  while m > 0 do
+  begin
+    for i := Low(c) to High(c) do 
+      c[i] := 0;
+
+    for i := 0 to n - 1 do 
+    begin
+      t := a[i] div (1 * (2 shl e)) and 15; 
+      Inc(c[t]);
+    end;
+
+    for i := 1 to 15 do 
+      Inc(c[i], c[i - 1]);
+
+    for i := n - 1 downto 0 do 
+    begin
+      t := a[i] div (1 * (2 shl e)) and 15; 
+      Dec(c[t]);
+      b[c[t]] := a[i];
+    end;
+
+    for i := 0 to n - 1 do 
+      a[i] := b[i];
+      Inc(e, K);
+      m := m div (1 * (2 shl K));  
+  end;
+end; 
+
+begin
+  Assign(inputFile, 'rawr.txt');
+  Reset(inputFile);
+  Read(inputFile, n);
+  Read(inputFile, m);
+  a[0] := m; 
+  for i := 1 to n - 1 do 
+  begin
+    Read(inputFile, a[i]);
+    if m < a[i] then
+      m := a[i];
+  end;
+  Close(inputFile); 
+
+  e := 0;
+  RadixSort(i);
+
+  Assign(outputFile, 'savout.txt'); 
+  Rewrite(outputFile);
+  for i := 0 to n - 1 do
+    Write(outputFile, a[i], ' ');
+  WriteLn(outputFile);
+  Close(outputFile); 
+  Writeln('Успешео сохранен результат в файл savout.txt');
+end.
